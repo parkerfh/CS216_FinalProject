b7557d8145980306e2f36448af05f169db1cc768
rebeccaerenbaum <rebecca.erenbaum@duke.edu>
2021-04-27 15:39:37 -0400

    confusion

--- national-model.R
+++ national-model.R
@@ -16,7 +16,7 @@ ellipse::pairs(as.factor(abortion_status) ~ ad_num_waiting16 + ad_num_adopted16 
                data = data, lower.panel = NULL) #lin relation btwn ad_num_waiting16 & ad_num_adopted16 and ad_num_waiting16 & ad_num_served16 and ad_num_adopted16 & ad_num_served16 
 
 full <- multinom(as.factor(abortion_status) ~ fp_perc_need_met16 + fp_num_served16 + fp_num_u20_served16 + fp_num_centers15 
-            + fp_total_expend15 + STATEFP20 + ad_num_waiting16 + ad_num_adopted16+ad_num_served16+
+            + fp_total_expend15 + ad_num_waiting16 + ad_num_adopted16+ad_num_served16+
               ad_prop_adopted16+parental_leave_score+prop_uninsured16 + fp_num_served16*fp_num_u20_served16 + 
               fp_num_centers15*fp_total_expend15 + ad_num_waiting16*ad_num_adopted16 + ad_num_waiting16*ad_num_served16
             + ad_num_adopted16*ad_num_served16, data = data)
@@ -28,9 +28,9 @@ kable(confint(full), format = "markdown", digits = 3, caption = "Full Model Conf
 regfit_backward <- step(full, direction = "backward")
 
 
-best_model <- multinom(as.factor(abortion_status) ~ ad_num_waiting16*ad_num_adopted16 + ad_num_adopted16*ad_num_served16
-                       + prop_uninsured16 + ad_prop_adopted16 + STATEFP20 + fp_num_served16*fp_num_u20_served16
-                       + fp_total_expend15 + parental_leave_score + fp_perc_need_met16 + fp_num_centers15, data = data)
+best_model <- multinom(as.factor(abortion_status) ~ ad_num_waiting16*ad_num_adopted16 + ad_num_adopted16*ad_num_served16 + 
+                         ad_num_waiting16:ad_num_served16 + prop_uninsured16 + ad_prop_adopted16 + fp_num_served16*fp_num_u20_served16
+                       + fp_total_expend15 + parental_leave_score + fp_perc_need_met16, data = data)
 
 kable(tidy(best_model), 
       format = "markdown", digits = 3, caption = "Best Model") 
@@ -38,5 +38,14 @@ kable(confint(best_model), format = "markdown", digits = 3, caption = "Best Mode
 
 # prediction of model
 pred <- predict(best_model, type = "class")
-table(data$abortion_status, pred)
+table <- table(data$abortion_status, pred)
+confusion_matrix <- as.data.frame(table)
 
+ggplot(data = confusion_matrix,
+       mapping = aes(x = Var1,
+                     y = pred)) +
+  geom_tile(aes(fill = Freq)) +
+  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+  scale_fill_gradient(low = "blue",
+                      high = "red",
+                      trans = "log")
